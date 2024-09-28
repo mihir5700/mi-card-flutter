29f10928ed4cd112dc2b83ef3de9c2d3d7eadfd5
Mihir Gupta <mihirgupta84@gmail.com>
2024-09-28 13:31:46 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+void main() => runApp (const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Constant constructor to uniquely identify this Widget
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          
+          appBar: AppBar(
+            backgroundColor: Colors.yellow[100],
+            title: const Text('MI-CARD LAYOUT APP'),
+          ),
+
+          //SINGLE CHILD LAYOUT WIDGET
+          body: SafeArea(
+            child: Container(
+              color:  Colors.white,
+              width: 100.0,
+              height: 100.0,
+              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0), //all, only, fromLTRB, etc
+              padding: const EdgeInsets.all(10.0),
+              child: const Text('SINGLE CHILD'),
+            ),
+          )
+
+        ),
+    );
+  }
+}
